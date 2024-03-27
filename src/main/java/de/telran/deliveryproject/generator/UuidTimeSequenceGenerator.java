d3d0eded70700a7b3335cb13302db6332d3a042c
Viktor Bulatov <lightoton@gmail.com>
2024-03-28 00:11:33 +0100

    update UUID generator

--- src/main/java/de/telran/deliveryproject/generator/UuidTimeSequenceGenerator.java
+++ src/main/java/de/telran/deliveryproject/generator/UuidTimeSequenceGenerator.java
@@ -10,22 +10,23 @@ import java.util.UUID;
 
 @RequiredArgsConstructor
 public class UuidTimeSequenceGenerator implements IdentifierGenerator {
-
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
         long currTimeMillis = System.currentTimeMillis();
-        UUID uuid = UUID.randomUUID();
 
-        return concatInHexFormat(currTimeMillis, uuid);
+        return concatUUIDAndTime(currTimeMillis, UUID.randomUUID());
     }
-
-    private char[] concatInHexFormat(long currTimeMillis, UUID uuid) {
-        String uuidStr = uuid.toString().replace("-", "");
+    private UUID concatUUIDAndTime(long currTimeMillis, UUID uuid) {
         String millisHex = Long.toHexString(currTimeMillis);
-        String sequenceHex = uuidStr.substring(0, 16);
-
-        String concatenated = millisHex + sequenceHex;
-
-        return concatenated.toCharArray();
+        // Преобразование UUID в строку без дефисов и сокращение до первых 16 символов
+        String uuidStr = uuid.toString().replace("-", "").substring(0, 16);
+        // Форматирование строки для обеспечения фиксированной длины
+        String concatenated = String.format("%016x%s", Long.parseLong(millisHex, 16), uuidStr);
+        String concatenatedWithDashes = concatenated.substring(0, 8) + "-" +
+                concatenated.substring(8, 12) + "-" +
+                concatenated.substring(12, 16) + "-" +
+                concatenated.substring(16, 20) + "-" +
+                concatenated.substring(20);
+        return UUID.fromString(concatenatedWithDashes);
     }
 }
