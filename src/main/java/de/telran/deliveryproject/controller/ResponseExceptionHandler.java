0568010c2ecca1f7c02599ddb903c19feb75c8aa
Viktor Bulatov <lightoton@gmail.com>
2024-04-02 01:44:24 +0200

    clean class

--- src/main/java/de/telran/deliveryproject/controller/ResponseExceptionHandler.java
+++ src/main/java/de/telran/deliveryproject/controller/ResponseExceptionHandler.java
@@ -2,13 +2,11 @@ package de.telran.deliveryproject.controller;
 
 import de.telran.deliveryproject.exception.ErrorExtension;
 import de.telran.deliveryproject.exception.UserInformationNotFoundException;
-import de.telran.deliveryproject.exception.errorMessege.ErrorMessage;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 @RestControllerAdvice
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
