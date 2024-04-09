3ede1553055e48de1cdfa2d14cdf559d9fd416f0
Viktor Bulatov <lightoton@gmail.com>
2024-04-09 10:17:06 +0200

    updated UserInfoController

--- src/main/java/de/telran/deliveryproject/controller/UserInfoController.java
+++ src/main/java/de/telran/deliveryproject/controller/UserInfoController.java
@@ -7,13 +7,13 @@ import de.telran.deliveryproject.entity.UserInfo;
 import de.telran.deliveryproject.service.interfaces.UserInfoService;
 import de.telran.deliveryproject.validation.annotation.UuidChecker;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
